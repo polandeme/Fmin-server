4788e8cc3263440c37da4dfbbf76e566131987b1
polandeme <polandeme@gmail.com>
2015-12-30 00:03:04 +0800

    draw img

--- api/v1/img.js
+++ api/v1/img.js
@@ -6,9 +6,9 @@ var parse = require('url').parse;
 var fs = require('fs');
 
 exports.maxScore = function(req, resss, next) {
-	var canvas = new Canvas(320, 320);
+	var canvas = new Canvas(640, 1136);
   	var ctx = canvas.getContext('2d');
-    var url = 'https://api.500px.com/v1/photos?feature=popular&consumer_key=zIhfdhAQ2qM2gHG4q8I1EqcU9AenE7wrYNCzRtA5&sort=votes_count&image_size=600';
+    var url = 'https://api.500px.com/v1/photos?feature=popular&consumer_key=zIhfdhAQ2qM2gHG4q8I1EqcU9AenE7wrYNCzRtA5&sort=votes_count&image_size=4';
   	var consumenrKey = 'zIhfdhAQ2qM2gHG4q8I1EqcU9AenE7wrYNCzRtA5';
   	request({
         url: url
@@ -28,6 +28,7 @@ exports.maxScore = function(req, resss, next) {
                     console.log('save success');
                     drawImg(ctx, canvas);
                     resss.send(sbody);
+                    // resss.send('<img src="' + canvas.toDataURL() + '">');
                 }
             })
         })
@@ -40,11 +41,15 @@ function drawImg(ctx, canvas) {
     var squid = fs.readFileSync(__dirname + '/test.jpg');
     img = new Image;
     img.src = squid;
-    ctx.drawImage(img, 30, 50, img.width / 4, img.height / 4);
+    ctx.drawImage(img, 20, 0, img.width * 2/3, img.height * 2/3);
+    ctx.font = "bold 16px Arial";
+    ctx.textAlign = 'center';
+    var text = '我是不是很聪明!';
+    ctx.fillText(text, 300, 316);
 
-    var out = fs.createWriteStream(__dirname + '/image-src.png')
-      , stream = canvas.createPNGStream();
 
+    var out = fs.createWriteStream(__dirname + '/image-src.jpg')
+      , stream = canvas.createJPEGStream();
     stream.on('data', function(chunk){
       out.write(chunk);
     });
